f338e41d2f0782d73e299453b8d86dff77a8c29a
Juliana Valle <juju.pvg@gmail.com>
2022-02-22 23:13:19 -0300

    question 1

---
+++ Lista 3/q1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <math.h>       
+
+using namespace std;
+
+
+int main(){
+
+	int x;
+
+	cin >> x;
+
+	if(x == 0){
+		cout << "1";
+	}
+	else{
+		cout << "0";
+	}
+	
+
+	return 0;
+}
